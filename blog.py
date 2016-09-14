8855f01783b4448dc4e9922d84cb422e8f18bd50
Jiri Prihoda <Jirkaprihoda@gmil.com>
2016-09-14 15:04:02 +0200

    index redirects to specific comic, next and previous functionality, comic button functionality, changed folder structure

--- blog.py
+++ blog.py
@@ -3,8 +3,8 @@ import model #will work with database
 
 urls = (
 	'/', 'Index',
-	'/comics/(\d+)', 'Comics'
-	'/static/', 'static'
+	'/(\d+)', 'Comics',
+	'/static/', 'static',
 	)
 
 t_globals ={
@@ -16,18 +16,31 @@ render = web.template.render('templates', base='base', globals=t_globals)
 
 class Index:
 	def GET(self):
-		latest_post = model.get_latest_post()
-		return render.index(latest_post)
+		post = model.get_latest_post()
+		post_id = int(post.id)
+		current_post = "/%d" %post_id
+		raise web.seeother(current_post)
 
+class Comics:
+	def GET(self,id):
+		post = model.get_post(int(id))
+		return render.comics(post)
 
 
 '''
 class Comics: #vezmi id od komiksu co je prave zobrazen a pricti/odecti jednicku
-	def GET_next(self, id):
-	current_post = 
+	def GET(self, id):
+		refering_url = web.ctx.path.get()
+		print refering_url
+		if refering_url == 'index' :#if current page is index
+			latest_post = model.get_latest_post_id(int(id))#go to latest post -1
+			post = latest_post - 1 + '.png'
+			return render.comics(post)
+		else: # go to current post -1
+			print "not implemented"
 
-	def GET_previous(self, id):
-	current_post = 
+	# def GET_previous(self, id):
+	# current_post = 
 '''
 
 '''
--- model.py
+++ model.py
@@ -4,6 +4,27 @@ db = web.database(dbn='mysql', db='dbname', user='root', passwd='password')
 
 
 def get_latest_post():
+	try:
+		return db.select('posts', order='id DESC', limit=1)[0]
+	except IndexError:
+		return None
+
+def get_post(id):
+	try:
+		return db.select('posts', order='id DESC', where='id=$id', vars=locals())[0]
+	except IndexError:
+		return None
+
+'''
+def get_second_latest_post():
 	lines = list(db.select('posts', order='id DESC'))
-	print  lines[0].post_image
-	return  lines[0].post_image
+	print  lines[0].id
+	latest =  lines[0].id
+	second_latest_id = latest - 1
+	second_latest = list(db.select('posts', order='id DESC'))
+	second_latest_post = second_latest[1].post_image
+'''
+'''
+get_latest_post()
+print list(db.select('posts', order='id DESC', limit=1))
+'''
